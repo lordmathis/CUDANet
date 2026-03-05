55f610c5f79cfa7d3c47d9c554a3af02668ac87e
LordMathis <matus@namesny.com>
2026-03-05 22:11:32 +0100

    Implement vsc vec add tests

--- test/cuda/test_matmul.cu
+++ test/cuda/test_matmul.cu
@@ -3,6 +3,13 @@
 
 #include "test_utils.hpp"
 
+/*
+
+Mat Vec Mul
+
+*/
+
+
 struct MatVecMulParams
 {
     CUDANet::DType dtype;
@@ -69,7 +76,7 @@ TEST_P(MatVecMulTest, MatrixVectorMultiplication) {
     }
 }
 
-std::vector<MatVecMulParams> initialize_params() {
+std::vector<MatVecMulParams> initialize_mat_vec_mul_params() {
     std::vector<std::vector<std::string>> rows = load_csv(FIXTURE_PATH + "/matmul/mat_vec_mul/metadata.csv");
 
     std::vector<MatVecMulParams> params;
@@ -99,5 +106,104 @@ std::vector<MatVecMulParams> initialize_params() {
 INSTANTIATE_TEST_SUITE_P(  
     MatVecMulTestCases,  
     MatVecMulTest,  
-    testing::ValuesIn(initialize_params())
+    testing::ValuesIn(initialize_mat_vec_mul_params())
+); 
+
+/*
+
+Vec Vec Add
+
+*/
+
+struct VecVecAddParams
+{
+    CUDANet::DType dtype;
+    const size_t size;
+    std::string vec_a_path;
+    std::string vec_b_path;
+    std::string expected_path;
+};
+
+class VecVecAddTest : public ::testing::TestWithParam<VecVecAddParams> {};
+
+template<typename T>
+void run_vec_vec_add_test(const VecVecAddParams params) {
+    auto vector_a_data = load_binary<T>(params.vec_a_path);
+    auto vector_b_data = load_binary<T>(params.vec_b_path);
+    auto expected_data = load_binary<T>(params.expected_path);
+
+    auto backend = CUDANet::BackendFactory::create(CUDANet::BackendType::CUDA_BACKEND, CUDANet::BackendConfig());
+
+    auto vector_shape = CUDANet::Shape{params.size};
+
+    auto vector_a = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    vector_a.set_data(vector_a_data.data());
+
+    auto vector_b = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    vector_b.set_data(vector_b_data.data());
+
+    auto expected = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    expected.set_data(expected_data.data());
+
+    auto output = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    output.zero();
+
+    auto grid_size =
+        (params.size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    CUDANet::Kernels::vec_vec_add<<<grid_size, BLOCK_SIZE>>>(
+        static_cast<const T*>(vector_a.device_ptr()),
+        static_cast<const T*>(vector_b.device_ptr()),
+        static_cast<T*>(output.device_ptr()),
+        params.size
+    );
+
+    std::vector<T> h_output = output.to_host<T>();
+    std::vector<T> h_expected = expected.to_host<T>();
+
+    ASSERT_EQ(h_output.size(), h_expected.size());
+
+    assert_elements_near(h_output, h_expected);
+}
+
+template void run_vec_vec_add_test<float>(const VecVecAddParams params);
+
+TEST_P(VecVecAddTest, VectorVectorAddition) {  
+    auto param = GetParam(); 
+
+    // DType dispatch
+    if (param.dtype == CUDANet::DType::FLOAT32) {
+        run_vec_vec_add_test<float>(param);
+    }
+}
+
+std::vector<VecVecAddParams> initialize_vec_vec_add_params() {
+    std::vector<std::vector<std::string>> rows = load_csv(FIXTURE_PATH + "/matmul/vec_vec_add/metadata.csv");
+
+    std::vector<VecVecAddParams> params;
+
+    for (const auto& row : rows) {
+        CUDANet::DType dtype = (row[0] == "float32")
+            ? CUDANet::DType::FLOAT32
+            : throw std::runtime_error("Unknown dtype: " + row[0]);
+
+        size_t size = std::stoul(row[1]);
+
+        params.push_back(VecVecAddParams{
+            dtype,
+            size,
+            row[2],
+            row[3],
+            row[4]
+        });
+    }
+
+    return params;
+}
+
+// Instantiate with test cases  
+INSTANTIATE_TEST_SUITE_P(  
+    VecVecAddTestCases,  
+    VecVecAddTest,  
+    testing::ValuesIn(initialize_vec_vec_add_params())
 ); 
--- test/generators/gen/cuda/matmul.py
+++ test/generators/gen/cuda/matmul.py
@@ -12,6 +12,7 @@ class MatMulGenerator(BaseGenerator):
 
     def generate(self):
         self._generate_mat_vec_mul(self.fixtures_path / "mat_vec_mul")
+        self._generate_vec_vec_add(self.fixtures_path / "vec_vec_add")
 
     def _generate_mat_vec_mul(self, save_path):
         os.makedirs(save_path, exist_ok=True)
@@ -38,3 +39,30 @@ class MatMulGenerator(BaseGenerator):
                 i += 1
 
         self.save_metadata(metadata, save_path / "metadata.csv")
+
+    def _generate_vec_vec_add(self, save_path):
+        os.makedirs(save_path, exist_ok=True)
+
+        i = 0
+        metadata = []
+        for size in [5, 256, 512]:
+            for dtype in ['float32']:
+
+                vec_a = torch.rand(size)
+                vec_a_save_path = save_path / f"{i}_vector_a.bin"
+                self.save_tensor(vec_a, vec_a_save_path)
+
+                vec_b = torch.rand(size)
+                vec_b_save_path = save_path / f"{i}_vector_b.bin"
+                self.save_tensor(vec_b, vec_b_save_path)
+
+                expected = torch.add(vec_a, vec_b)
+                expected_save_path = save_path / f"{i}_expected.bin"
+                self.save_tensor(expected, expected_save_path)
+
+                metadata.append([dtype, str(size), vec_a_save_path, vec_b_save_path, expected_save_path])
+
+                i += 1
+        
+        self.save_metadata(metadata, save_path / "metadata.csv")
+        
