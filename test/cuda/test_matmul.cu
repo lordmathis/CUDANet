ee6490cf3ccbfa93360c26981a76b254d979b902
LordMathis <matus@namesny.com>
2026-03-07 12:08:28 +0100

    Implement vec vec sub test

--- test/cuda/test_matmul.cu
+++ test/cuda/test_matmul.cu
@@ -24,7 +24,6 @@ class MatVecMulTest : public ::testing::TestWithParam<MatVecMulParams> {};
 
 template<typename T>
 void run_mat_vec_mul_test(const MatVecMulParams params) {
-    // Load binary data
     auto matrix_data = load_binary<T>(params.matrix_path);
     auto vector_data = load_binary<T>(params.vector_path);
     auto expected_data = load_binary<T>(params.expected_path);
@@ -71,7 +70,6 @@ template void run_mat_vec_mul_test<float>(const MatVecMulParams params);
 TEST_P(MatVecMulTest, MatrixVectorMultiplication) {  
     auto param = GetParam(); 
 
-    // DType dispatch
     if (param.dtype == CUDANet::DType::FLOAT32) {
         run_mat_vec_mul_test<float>(param);
     }
@@ -103,7 +101,6 @@ std::vector<MatVecMulParams> initialize_mat_vec_mul_params() {
     return params;
 }
 
-// Instantiate with test cases  
 INSTANTIATE_TEST_SUITE_P(  
     MatVecMulTestCases,  
     MatVecMulTest,  
@@ -173,7 +170,6 @@ template void run_vec_vec_add_test<float>(const VecVecAddParams params);
 TEST_P(VecVecAddTest, VectorVectorAddition) {  
     auto param = GetParam(); 
 
-    // DType dispatch
     if (param.dtype == CUDANet::DType::FLOAT32) {
         run_vec_vec_add_test<float>(param);
     }
@@ -203,9 +199,107 @@ std::vector<VecVecAddParams> initialize_vec_vec_add_params() {
     return params;
 }
 
-// Instantiate with test cases  
 INSTANTIATE_TEST_SUITE_P(  
     VecVecAddTestCases,  
     VecVecAddTest,  
     testing::ValuesIn(initialize_vec_vec_add_params())
-); 
+);
+
+/*
+
+Vec Vec Sub
+
+*/
+
+struct VecVecSubParams
+{
+    CUDANet::DType dtype;
+    const size_t size;
+    std::string vec_a_path;
+    std::string vec_b_path;
+    std::string expected_path;
+};
+
+class VecVecSubTest : public ::testing::TestWithParam<VecVecSubParams> {};
+
+template<typename T>
+void run_vec_vec_sub_test(const VecVecSubParams params) {
+    auto vector_a_data = load_binary<T>(params.vec_a_path);
+    auto vector_b_data = load_binary<T>(params.vec_b_path);
+    auto expected_data = load_binary<T>(params.expected_path);
+
+    auto backend = CUDANet::BackendFactory::create(CUDANet::BackendType::CUDA_BACKEND, CUDANet::BackendConfig());
+
+    auto vector_shape = CUDANet::Shape{params.size};
+
+    auto vector_a = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    vector_a.set_data(vector_a_data.data());
+
+    auto vector_b = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    vector_b.set_data(vector_b_data.data());
+
+    auto expected = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    expected.set_data(expected_data.data());
+
+    auto output = CUDANet::Tensor(vector_shape, params.dtype, backend.get());
+    output.zero();
+
+    auto grid_size =
+        (params.size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    CUDANet::Kernels::vec_vec_sub<<<grid_size, BLOCK_SIZE>>>(
+        static_cast<const T*>(vector_a.device_ptr()),
+        static_cast<const T*>(vector_b.device_ptr()),
+        static_cast<T*>(output.device_ptr()),
+        params.size
+    );
+    cudaDeviceSynchronize();
+
+    std::vector<T> h_output = output.to_host<T>();
+    std::vector<T> h_expected = expected.to_host<T>();
+
+    ASSERT_EQ(h_output.size(), h_expected.size());
+
+    assert_elements_near(h_output, h_expected);
+}
+
+template void run_vec_vec_sub_test<float>(const VecVecSubParams params);
+
+TEST_P(VecVecSubTest, VectorVectorSubtraction) {  
+    auto param = GetParam(); 
+
+    if (param.dtype == CUDANet::DType::FLOAT32) {
+        run_vec_vec_sub_test<float>(param);
+    }
+}
+
+std::vector<VecVecSubParams> initialize_vec_vec_sub_params() {
+    std::vector<std::vector<std::string>> rows = load_csv(FIXTURE_PATH + "/matmul/vec_vec_sub/metadata.csv");
+
+    std::vector<VecVecSubParams> params;
+
+    for (const auto& row : rows) {
+        CUDANet::DType dtype = (row[0] == "float32")
+            ? CUDANet::DType::FLOAT32
+            : throw std::runtime_error("Unknown dtype: " + row[0]);
+
+        size_t size = std::stoul(row[1]);
+
+        params.push_back(VecVecSubParams{
+            dtype,
+            size,
+            row[2],
+            row[3],
+            row[4]
+        });
+    }
+
+    return params;
+}
+
+// Instantiate with test cases  
+INSTANTIATE_TEST_SUITE_P(  
+    VecVecSubTestCases,  
+    VecVecSubTest,  
+    testing::ValuesIn(initialize_vec_vec_sub_params())
+);
--- test/generators/gen/cuda/matmul.py
+++ test/generators/gen/cuda/matmul.py
@@ -13,6 +13,7 @@ class MatMulGenerator(BaseGenerator):
     def generate(self):
         self._generate_mat_vec_mul(self.fixtures_path / "mat_vec_mul")
         self._generate_vec_vec_add(self.fixtures_path / "vec_vec_add")
+        self._generate_vec_vec_sub(self.fixtures_path / "vec_vec_sub")
 
     def _generate_mat_vec_mul(self, save_path):
         os.makedirs(save_path, exist_ok=True)
@@ -57,6 +58,32 @@ class MatMulGenerator(BaseGenerator):
                 self.save_tensor(vec_b, vec_b_save_path)
 
                 expected = torch.add(vec_a, vec_b)
+                expected_save_path = save_path / f"{i}_expected.bin"
+                self.save_tensor(expected, expected_save_path)
+
+                metadata.append([dtype, str(size), vec_a_save_path, vec_b_save_path, expected_save_path])
+
+                i += 1
+        
+        self.save_metadata(metadata, save_path / "metadata.csv")
+
+    def _generate_vec_vec_sub(self, save_path):
+        os.makedirs(save_path, exist_ok=True)
+
+        i = 0
+        metadata = []
+        for size in [5, 512, 1024]:
+            for dtype in ['float32']:
+
+                vec_a = torch.rand(size)
+                vec_a_save_path = save_path / f"{i}_vector_a.bin"
+                self.save_tensor(vec_a, vec_a_save_path)
+
+                vec_b = torch.rand(size)
+                vec_b_save_path = save_path / f"{i}_vector_b.bin"
+                self.save_tensor(vec_b, vec_b_save_path)
+
+                expected = torch.sub(vec_a, vec_b)
                 expected_save_path = save_path / f"{i}_expected.bin"
                 self.save_tensor(expected, expected_save_path)
 
